5d214a8c2d6b5f95ad21e48dc3822c4c7832e3fa
srconst123 <86607104+srconst123@users.noreply.github.com>
2022-10-10 14:01:00 +0530

    Update Footer.js
    dfsf

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -14,12 +14,25 @@ const Footer = () => {
 
   const submitEnquiry = () => {
 
-    Axios.post('https://srconst.000webhostapp.com/api/enquiry.php', {
+//     Axios.post('https://srconst.000webhostapp.com/api/enquiry.php', {
+//       userName: userName,
+//       userEmail: userEmail,
+//       userMobile: userMobile,
+//       userMessage: userMessage
+//     })
+  fetch('https://srconst.000webhostapp.com/api/enquiry.php', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
       userName: userName,
       userEmail: userEmail,
       userMobile: userMobile,
       userMessage: userMessage
-    })
+  })
+});
   }
   const form = useRef();
 
