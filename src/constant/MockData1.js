bb8dabf362205c5e0f8a58b036cbb3a9382e0a81
srconst123 <86607104+srconst123@users.noreply.github.com>
2022-11-09 16:13:10 +0530

    Add files via upload
    ff5

---
+++ src/constant/MockData1.js
@@ -0,0 +1,78 @@
+export const ccp1 = [
+    {
+        url: "assets/images/a4.jpg",
+        title: "Sri Ram Krishna Enclave",
+        description: "This site is located in Green Park Area, 350m from Manya Place, Morabadi, Ranchi."
+    }
+]
+export const ccp2 = [
+    {
+        url: "assets/images/a5.jpg",
+        title: "Sri Ram Tower (Phase I)",
+        description: "This commercial cum residential tower is located in Gandhi Road, Dhanbad."
+    }
+]
+export const ccp3 = [
+    {
+        url: "assets/images/sriram.jpg",
+        title: "Sri Ram Apartment",
+        description: "This Apartment is located in Ranchi"
+    }
+]
+export const ccp4 = [
+    {
+        url: "assets/images/rrs.jpg",
+        title: "Construction Of R & R Colony NTPC At Residential & None Residential Buildings",
+        description: " This site is located in Barkagoan, Hazaribag, Jharkhand (Client-MBECL, Kolkata)"
+    }
+]
+export const ccp5 = [
+    {
+        url: "assets/images/sitakunjk.jpg",
+        title: "Sita Kunj Karamtoli, Ranchi",
+        description: "These apartments were made exclusively for the directors of Sri Ram construction group and therefor the location and apartments still command a premium in the area of Ranchi"
+    }
+]
+export const ccp6 = [
+    {
+        url: "assets/images/relect.jpg",
+        title: "Rural Electrification work of East singhbum,Jharkhand",
+        description: " These Projects are completed by Shri Ram Construction"
+    }
+]
+export const ccp7 = [
+    {
+        url: "assets/images/10kmpcc.jpg",
+        title: "10 km PCC Road and Boundary wall at jharsiguda,  Orrisa",
+        description: " These Projects are completed by Shri Ram Construction"
+    }
+]
+export const ccp8 = [
+    {
+        url: "assets/images/rrc.jpg",
+        title: "Construction of R & R colony NTPC at Road ,Culvet and Drainagework BarkaGaon ,Hazaribagh (Client - MBECL,Kolkata",
+        description: " These Projects are completed by Shri Ram Construction"
+    }
+]
+export const ccp9 = [
+    {
+        url: "assets/images/hsms.jpg",
+        title: "NTPC Barkagaon Health center ",
+        description: " These Projects are completed by Shri Ram Construction"
+    }
+]
+export const ccp10 = [
+    {
+        url: "assets/images/ms.jpg",
+        title: "NTPC Barkagaon  Middle School",
+        description: " These Projects are completed by Shri Ram Construction"
+    }
+]
+export const ccp11 = [
+    {
+        url: "assets/images/school.jpg",
+        title: "NTPC Barkagaon Sr. Secondary School",
+        description: " These Projects are completed by Shri Ram Construction"
+    }
+
+]
