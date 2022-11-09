3aeadbf463e419dea586312fbe580ebd48e1d864
srconst123 <86607104+srconst123@users.noreply.github.com>
2022-11-09 15:10:49 +0530

    Create ccard.js
    ddd

---
+++ src/component/cards/ccard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import "./Cards.css";
+
+
+const Cards = ({ data}) => {
+  return (
+    <
+    <Container className='parent-card-container'>
+      <div className='parent-card-group'>
+        {
+          data && data.map(obj => {
+            const { url, title, description, imgHeight } = obj;
+            return (
+              <Card key={title} >
+                <Card.Img variant="top" src={url} height={imgHeight} />
+                <Card.Body>
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Text>{description}</Card.Text> 
+                </Card.Body>
+              </Card>
+            );
+          })
+        }
+      </div>
+    </Container>
+  )
+}
+
+export default Cards
